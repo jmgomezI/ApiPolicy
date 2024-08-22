31fdb7d51215dace5af9fd34f6491704bf567ecc
jmgomezI <jmgomez@izertis.com>
2024-08-22 13:16:32 +0200

    Cleaning code

--- src/main/java/jmgomez/apipolicy/service/IPolicyService.java
+++ src/main/java/jmgomez/apipolicy/service/IPolicyService.java
@@ -1,7 +1,5 @@
 package jmgomez.apipolicy.service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import jmgomez.apipolicy.model.Policy;
 import jmgomez.apipolicy.model.Accident;
 import jmgomez.apipolicy.model.dto.PolicyDto;
 import jmgomez.apipolicy.model.dto.PolicyDtoCov;
